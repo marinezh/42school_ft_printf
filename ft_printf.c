6cc8a7445af6d7b7c2e24a9dfb06559e4212a903
Marina Zhivotova <mazhivotova@gmail.com>
2024-12-09 18:10:58 +0200

    add ft_putchar and putstr

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,41 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 12:39:37 by mzhivoto          #+#    #+#             */
-/*   Updated: 2024/12/02 15:20:05 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2024/12/09 17:57:07 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+    int len;
+    len = 0;
+    while (str[len])
+    {
+        len++;
+    }
+    write(1, str, len);
+}
+// void ft_putchar(char c)
+// {
+//     write(1, &c, 1);
+// }
+
+int ft_putchar(char c)
+{
+    if (write(1, &c, 1) != 1)
+    {
+        return -1; // Return -1 on failure (similar to EOF)
+    }
+    return (unsigned char)c;
+}
 
 void mini_printf(const char *format, ...)
 {
@@ -35,15 +64,22 @@ void mini_printf(const char *format, ...)
         }
         else
         {
-            putchar(*format);
+            ft_putchar(*format);
         }
         format++;
     }
 
     va_end(args);
 }
+
 int main(void)
 {
-    mini_printf("Hello %s, number is %d\n", "World", 42);
+    char *s = "cspdiuxX";
+    int num = 42;
+    mini_printf("Hello %s, number is %d\n", "World", num);
+    // mini_printf("string %s", s);
+    // ft_putstr("hello\n");
+    // ft_putstr("hi\n");
+    // ft_putstr("cspdiuxX%\n");
     return (0);
 }
