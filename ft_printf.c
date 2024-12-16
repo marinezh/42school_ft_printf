24167a4c124d18abbacad5b69fbd545b14641844
Marina Zhivotova <mazhivotova@gmail.com>
2024-12-16 18:27:14 +0200

    add some code to my printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 12:39:37 by mzhivoto          #+#    #+#             */
-/*   Updated: 2024/12/09 17:57:07 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2024/12/16 17:58:44 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,26 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void ft_putnbr(int nb)
+{
+    unsigned int nbr;
+
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        nbr = nb * -1;
+    }
+    else
+        nbr = nb;
+    if (nbr >= 10)
+        ft_putnbr(nbr / 10);
+    ft_putchar(nbr % 10 + '0');
+}
+
 void ft_putstr(char *str)
 {
     int len;
@@ -28,38 +48,34 @@ void ft_putstr(char *str)
     }
     write(1, str, len);
 }
-// void ft_putchar(char c)
-// {
-//     write(1, &c, 1);
-// }
-
-int ft_putchar(char c)
-{
-    if (write(1, &c, 1) != 1)
-    {
-        return -1; // Return -1 on failure (similar to EOF)
-    }
-    return (unsigned char)c;
-}
 
 void mini_printf(const char *format, ...)
 {
     va_list args;
 
     va_start(args, format);
-
+    // printf("test1: %c\n", format);
     while (*format)
     {
+        // printf("test3: %c\n", format);
         if (*format == '%')
         {
+
             format++;
             if (*format == 'd')
             {
-                printf("%d", va_arg(args, int));
+                int num = va_arg(args, int);
+                ft_putnbr(num);
             }
             else if (*format == 's')
             {
-                printf("%s", va_arg(args, char *));
+                char *str = va_arg(args, char *);
+                ft_putstr(str);
+            }
+            else
+            {
+                ft_putchar('%'); // Print the '%' character
+                ft_putchar(*format);
             }
         }
         else
@@ -74,12 +90,19 @@ void mini_printf(const char *format, ...)
 
 int main(void)
 {
-    char *s = "cspdiuxX";
+    char *s = "abc";
+    // char k = 'c';
     int num = 42;
-    mini_printf("Hello %s, number is %d\n", "World", num);
-    // mini_printf("string %s", s);
+    printf("Hello %x, number %d\n", 87687678, num);
+    int result = printf("A%s, B %d\n", s, num);
+    printf("result %d\n", result);
+
+    // ft_putstr("hello\n");
     // ft_putstr("hello\n");
     // ft_putstr("hi\n");
-    // ft_putstr("cspdiuxX%\n");
+    // ft_putnbr(num);
+    // ft_putchar('\n');
+    // ft_putchar(k);
+    // ft_putchar('\n');
     return (0);
 }
---
+++ test.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 12:39:37 by mzhivoto          #+#    #+#             */
+/*   Updated: 2024/12/16 16:01:50 by mzhivoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+int ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void ft_putnbr(int nb)
+{
+    unsigned int nbr;
+
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        nbr = nb * -1;
+    }
+    else
+        nbr = nb;
+    if (nbr >= 10)
+        ft_putnbr(nbr / 10);
+    ft_putchar(nbr % 10 + '0');
+}
+
+// void ft_putstr(char *str)
+// {
+//     int len;
+//     len = 0;
+//     while (str[len])
+//     {
+//         len++;
+//     }
+//     write(1, str, len);
+// }
+
+int main(void)
+{
+    char *s = "cspdiuxX%";
+    char k = 'c';
+    int num = 42;
+    // ft_putstr("hello\n");
+    // ft_putstr("hi\n");
+    ft_putnbr(num);
+    ft_putchar('\n');
+    ft_putchar(k);
+    ft_putchar('\n');
+    return (0);
+}
