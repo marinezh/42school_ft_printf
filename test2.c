5c327d2caaf5b622d9f2922949730c92510886cb
Marina Zhivotova <mazhivotova@gmail.com>
2024-12-18 17:01:02 +0200

    add some staff

--- test2.c
+++ test2.c
@@ -40,46 +40,45 @@ int ft_printf(const char *format, ...)
     va_list args;
     int i = 0;
     va_start(args, format);
-    printf("Starting ft_printf with format: %s\n", format); // Debugging
-    printf("to see args: %s\n", format);                    // Debugging
+    // printf("Starting ft_printf with format: %s\n", format); // Debugging
 
     while (format[i])
     {
-        printf("Current character: %c\n", format[i]); // Debugging
+        // printf("Current character: %c\n", format[i]); // Debugging
         if (format[i] == '%')
         {
             format++;
-            printf("Found a format specifier: %c\n", *format); // Debugging
+            // printf("Found a format specifier: %c\n", format[i]); // Debugging
 
             if (format[i] == 'd')
             {
                 int num = va_arg(args, int);
-                printf("Extracted argument (integer): %d\n", num); // Debugging
+                // printf("Extracted argument (integer): %d\n", num); // Debugging
                 ft_putnbr(num);
             }
             else if (format[i] == 's')
             {
                 char *str = va_arg(args, char *);
-                printf("Extracted argument (string): %s\n", str); // Debugging
+                // printf("Extracted argument (string: %s\n", str); // Debugging
                 ft_putstr(str);
             }
             else
             {
-                printf("Unrecognized format specifier: %c\n", format[i]); // Debugging
-                ft_putchar('%');                                          // Print the '%' character
-                ft_putchar(format[i]);
+                // printf("Unrecognized format specifier: %c\n", format[i]); // Debugging
+                ft_putchar('%'); // Print the '%' character
+                // ft_putchar(format[i]);
             }
         }
-        elsepwd
+        else
         {
-            printf("Printing character: %c\n", format[i]); // Debugging
+            // printf("Printing character: %c\n", format[i]); // Debugging
             ft_putchar(format[i]);
         }
         format++;
     }
 
     va_end(args);
-    printf("Finished processing format string.\n"); // Debugging
+    // printf("Finished processing format string.\n"); // Debugging
     return (0);
 }
 int main(void)
@@ -89,10 +88,9 @@ int main(void)
     int num = 42;
     int result = printf("Hello %s, ABC %d\n", s, num);
 
-    ft_printf("Hello %s, ABC%d", s, num);
+    ft_printf("cf: Hello %q, ABC %d\n", s, num);
 
-    printf("result %d\n", result);
-
+    printf("of: Hello %q, ABC %d\n", s, num);
     // ft_putstr("hello\n");
     // ft_putstr("hello\n");
     // ft_putstr("hi\n");
