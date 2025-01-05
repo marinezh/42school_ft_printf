1d6bc9df4ec4d3566bbe8b0aabdd38de382855b7
marinezh <mazhivotova@gmail.com>
2025-01-05 21:42:40 +0200

    change ft_printf fucntion

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 12:39:37 by mzhivoto          #+#    #+#             */
-/*   Updated: 2025/01/05 02:45:13 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2025/01/05 21:20:44 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,117 +15,65 @@
 #include <unistd.h>
 #include "ft_printf.h"
 
-static int ft_handle_format(const char **format, va_list args)
+static int ft_handle_format(const char format, va_list *args)
 {
     int chars_printed;
 
     chars_printed = 0;
-    if (**format == 'c')
-        chars_printed += ft_putchar(va_arg(args, int)); // Char is promoted to int
-    else if (**format == 'd' || **format == 'i')
-        chars_printed += ft_putnbr(va_arg(args, int)); // Integer argument
-    else if (**format == 's')
-        chars_printed += ft_putstr(va_arg(args, char *)); // String
-    else if (**format == 'u')
-        chars_printed += ft_put_unsigned(va_arg(args, unsigned int)); // Unsigned int
-    else if (**format == 'x')
-        chars_printed += ft_puthex(va_arg(args, unsigned int), 1); // Hexadecimal lowercase
-    else if (**format == 'X')
-        chars_printed += ft_puthex(va_arg(args, unsigned int), 0); // Hexadecimal uppercase
-    else if (**format == 'p')
-        chars_printed += ft_putptr(va_arg(args, void *)); // Pointer
-    else if (**format == '%')
-        chars_printed += ft_putchar('%'); // Handle % directly
+    if (format == 'c')
+        chars_printed += ft_putchar(va_arg(*args, int)); 
+    else if (format == 'd' || format == 'i')
+        chars_printed += ft_putnbr(va_arg(*args, int)); 
+    else if (format == 's')
+        chars_printed += ft_putstr(va_arg(*args, char *)); 
+    else if (format == 'u')
+        chars_printed += ft_put_unsigned(va_arg(*args, unsigned int)); 
+    else if (format == 'x')
+        chars_printed += ft_puthex(va_arg(*args, unsigned int), 1); 
+    else if (format == 'X')
+        chars_printed += ft_puthex(va_arg(*args, unsigned int), 0); 
+    else if (format == 'p')
+        chars_printed += ft_putptr(va_arg(*args, void *)); 
+    else if (format == '%')
+        chars_printed += ft_putchar('%'); 
     else
         chars_printed += ft_putchar('%'); // Handle invalid specifier by printing '%'
     
     return chars_printed;
 }
+static int ft_handle_type(char c)
+{
+    return (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || c == 'u'
+            || c == 'x' || c == 'X' || c == '%');
+}
 int ft_printf(const char *format, ...)
 {
     va_list args;
     int total_chars;
+    size_t i;
 
     total_chars = 0;
+    i = 0;
     va_start(args, format);
-
-    while (*format)
+    while (format[i])
     {
-        if (*format == '%')
+        if (format[i] == '%')
         {
-            format++;
-            total_chars += ft_handle_format(&format, args);
+            if (ft_handle_type(format[i + 1]))  // Check if the next character is a valid specifier
+            {
+                total_chars += ft_handle_format(format[i + 1], &args);  // Handle the argument based on specifier
+                i++;  // Skip the format specifier
+            }
         }
         else
-        {
-            total_chars += ft_putchar(*format);
-        }
-        format++;
+            total_chars += ft_putchar(format[i]);
+        i++;
     }
 
     va_end(args);
     return (total_chars);
 }
-// int ft_printf(const char *format, ...)
-// {
-//     va_list args;
 
-//     va_start(args, format);
-//     //printf("Starting ft_printf with format: %s\n", format); // Debugging
-//     while (*format)
-//     {
-//         // printf("test3: %c\n", format);
-//         //printf("Current character: %c\n", *format); // Debugging
-//         if (*format == '%')
-//         {
-//             format++;
-//             if (*format == 'c')
-//             {
-//                 return ft_putchar(va_arg(args, int));
-//             }
-//             if (*format == 'd' || *format == 'i')
-//             {
-//                 int num = va_arg(args, int);
-//                 return ft_putnbr(num);
-//             }
-//             if (*format == 's')
-//             {
-//                 char *str = va_arg(args, char *);
-//                 return ft_putstr(str);
-//             }
-//             if (*format == 'u')
-//             {
-//                 unsigned int num = va_arg(args, unsigned int);
-//                 return ft_put_unsigned(num);
-//             }
-//             if (*format == 'x')
-//             {
-//                 return ft_puthex(va_arg(args, unsigned int), 1);
-//             }
-//             if (*format == 'X')
-//             {
-//                 return ft_puthex(va_arg(args, unsigned int), 0);
-//             }
-//             if(*format == 'p')
-//             {
-//                 return ft_putptr(va_arg(args, void *));
-//             }
-//             else if (*format == '%')
-//             {
-//                 return ft_putchar('%'); // Print the '%' character
-//             //     // ft_putchar(*format);
-//             }
-//         }
-//         else
-//         {
-//             ft_putchar(*format);
-//         }
-//         format++;
-//     }
-
-//     va_end(args);
-//     return (0);
-// }
 
 int main(void)
 {
